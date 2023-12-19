f4aa2a9c004798e5c9c31153ecd7e3358e407275
Kelly Helmut Lord <helmut@helmutlord.com>
2023-12-18 19:20:40 -0500

    Added flash delay

--- flashing/flash.sh
+++ flashing/flash.sh
@@ -1,1 +1,1 @@
-openocd -f "interface/stlink-dap.cfg" -f "angry_miao.cfg" -c "transport select dapdirect_swd; source [find target/nrf52.cfg]; init; flash_bootloader;"
+openocd -f "interface/stlink-dap.cfg" -f "angry_miao.cfg" -c "transport select dapdirect_swd; source [find target/nrf52.cfg]; init; sleep 2000; flash_bootloader;"
